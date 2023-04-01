51c93578b0715613a721be6f37334f3152dae33d
Steven Quintana <mquintanar2@gmail.com>
2023-04-01 09:44:05 -0600

    Add project files.

---
+++ Clase1Abril/Program.cs
@@ -0,0 +1,33 @@
+﻿void matriz()
+{
+    string[] nombre = new string[5];
+
+    for (int i = 0; i < 5;  i++) {
+        Console.WriteLine("\nIngrese el nombre: " + (i+1));
+        nombre[i] = Console.ReadLine();
+    }
+
+    int[] notas = new int[nombre.Length];
+
+    for (int i = 0; i < nombre.Length; i++)
+    {
+        Console.WriteLine("\nIngrese la nota de " + nombre[i]);
+        notas[i] = Convert.ToInt32(Console.ReadLine());
+    }
+
+    int max = 0;
+    int num = 0;
+
+    for(int i = 0; i < notas.Length; i++)
+    {
+        if(notas[i] > max)
+        {
+            max = notas[i];
+            num = i;
+        }
+    }
+
+    Console.WriteLine("\nLa nota mayor es de: " + nombre[num] + " con " + max);
+}
+
+matriz();
